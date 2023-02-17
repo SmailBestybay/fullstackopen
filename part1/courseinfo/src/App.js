a7ad232cff145a187cf6fb3f9785636620b70649
Smile <smailbestybay@gmail.com>
2023-02-17 00:23:21 -0800

    complete exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,38 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+// const Part = () => {
+
+// }
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <p>Number of exercises {props.exercisesCount}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +44,13 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course='Half Stack application development' />
+      <Content 
+        part1='Fundamentals of React' exercises1='10' 
+        part2='Using props to pass data' exercises2='7'
+        part3='State of a component' exercises3='14'
+      />
+      <Total exercisesCount={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
