9df71a1f3cc25c8c361bbb3362c074221ba6d611
Smile <smailbestybay@gmail.com>
2023-02-20 14:16:59 -0800

    complete 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,6 +10,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all
   console.log(good, neutral, bad)
 
 
@@ -26,6 +29,9 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
       </div>
     </>
   )
