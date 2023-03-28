a969eef48a6170be3898523f503129322a700617
Smile <smailbestybay@gmail.com>
2023-03-28 01:47:36 -0700

    complete exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc += blog.likes, 0)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
