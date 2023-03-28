22930658148a4ef4397a664a0dfcfaa4e53d7ef1
Smile <smailbestybay@gmail.com>
2023-03-27 22:51:40 -0700

    refactor router into separate module

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,7 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
@@ -10,24 +10,9 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
