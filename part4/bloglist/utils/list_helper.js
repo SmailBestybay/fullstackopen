2b7ac27f7f59dd2e8077b780eacd138e1829a610
Smile <smailbestybay@gmail.com>
2023-03-30 17:39:55 -0700

    figured out why the the reduce was failing with a differnet ternerary statement.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,8 +6,14 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc += blog.likes, 0)
 
 const favoriteBlog = (blogs) => blogs.reduce((acc, blog) => {
-  return acc.likes > blog.likes ? acc : blog
-  // return acc.likes < blog.likes ? blog : acc
+  return acc.likes > blog.likes ? { ...acc } : { ...blog } // passes
+
+
+  // the statement below does not pass because undefined < or > any number is always false
+  // so on the first iteration of reduce the empty object is returned, and
+  // the pattern repeates.
+
+  // return acc.likes < blog.likes ? { ...blog } : { ...acc } // does not pass
 }, {})
 
 module.exports = {
