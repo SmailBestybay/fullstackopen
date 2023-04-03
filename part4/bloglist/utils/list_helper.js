990dda179f4237793ed4862f14b460f9734654ab
Smile <smailbestybay@gmail.com>
2023-04-02 20:43:34 -0700

    complete exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,14 +6,7 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc += blog.likes, 0)
 
 const favoriteBlog = (blogs) => blogs.reduce((acc, blog) => {
-  return acc.likes > blog.likes ? { ...acc } : { ...blog } // passes
-
-
-  // the statement below does not pass because undefined < or > any number is always false
-  // so on the first iteration of reduce the empty object is returned, and
-  // the pattern repeates.
-
-  // return acc.likes < blog.likes ? { ...blog } : { ...acc } // does not pass
+  return acc.likes > blog.likes ? { ...acc } : { ...blog }
 }, {})
 
 module.exports = {
