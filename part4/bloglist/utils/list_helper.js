031749f6b605b2bbc9f3715787b4deda0a738aba
Smile <smailbestybay@gmail.com>
2023-04-03 01:46:12 -0700

    exercise 4.6, good for now, revisit in the future

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,31 +10,29 @@ const favoriteBlog = (blogs) => blogs.reduce((acc, blog) => {
 }, {})
 
 /*
-first calculate how many blogs each author has.
-   reduce to an array where each element is in this form
+first, calculate how many blogs each author has.
+   reduce to an object where each element is in this form
   {
     author: "Robert C. Martin",
     blogs: 3
   }
   first acc is an object of of objects. This will let us look up existing authors at O(1)
 
-
-second fund the author with most blogs.
+second, find the author with most blogs.
   reduce again keeping the author with most blogs in the accumilator
 */
-
-const mostBlogs = (blogs) => Object.values(blogs.reduce((acc, blog) => {
-  const currCount = acc[blog.author] ? acc[blog.author].blogs : 0
-  return {
-    ...acc,
-    [blog.author]: {
-      author: blog.author,
-      blogs: currCount + 1
+const mostBlogs = (blogs) => Object.values(
+  blogs.reduce((acc, blog) => {
+    const currCount = acc[blog.author] ? acc[blog.author].blogs : 0
+    return {
+      ...acc,
+      [blog.author]: {
+        author: blog.author,
+        blogs: currCount + 1
+      }
     }
-  }
-}, {})).reduce((acc, author) => {
-  return author.blogs < acc.blogs ? acc : author
-}, {})
+  }, {})
+).reduce((acc, author) => author.blogs < acc.blogs ? acc : author, {})
 
 module.exports = {
   dummy,
