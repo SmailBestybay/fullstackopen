22ade81e1ccc228f8014eab5ef53d8255908613f
Smile <smailbestybay@gmail.com>
2023-04-14 22:57:53 -0700

    add password validation to userRouter.post

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if(password.length < 3) {
+    const error = Error('password must be at least 3 characters long')
+    error.name = 'PasswordValidationError'
+    throw error
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
