ff9e445ead84647d734ab2c59f05e446954d30b2
Smile <smailbestybay@gmail.com>
2023-05-29 20:11:20 -0700

    complete exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -25,9 +33,19 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
     } catch (exception) {
       console.log(exception.response.data.error)
     }
+  }
+
+  const hanldeLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -60,7 +78,10 @@ const App = () => {
   const blogsComponent = () => (
     <div>
       <h2>blogs</h2>
-      <div>{user.name} logged in</div>
+      <div>
+        {user.name} logged in 
+        <button onClick={hanldeLogout}>logout</button> 
+      </div>
       <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
