5f300c6a68eb1b782d8e44b6513cacb7a1e10864
Smile <smailbestybay@gmail.com>
2023-05-29 19:56:12 -0700

    complete exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,13 +15,66 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception.response.data.error)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+    <h2>Log in to application</h2>
+    <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const blogsComponent = () => (
     <div>
       <h2>blogs</h2>
+      <div>{user.name} logged in</div>
+      <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
+  )
+
+  return (
+    <>
+
+      {user === null && loginForm()}
+      {user !== null && blogsComponent()}
+      
+    </>
   )
 }
 
