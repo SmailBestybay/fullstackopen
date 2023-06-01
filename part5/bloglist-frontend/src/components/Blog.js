d006b593ecd3c0d6ca63c314dcf5299c3ff13292
Smile <smailbestybay@gmail.com>
2023-05-31 22:45:07 -0700

    complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -29,6 +29,7 @@ const Blog = ({ blog, blogs, setBlogs, user }) => {
     }
   }
 
+
   return (
     <>
       {visible ? (
--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('blog component', () => {
-  const blog = {
-    title: 'Test title',
-    author: 'Tester Author',
-    likes: 0,
-    user: {
-      name: 'Tester is the name',
-      username: 'Tester is the username',
-      id: '123123'
-    },
-    url: 'example@url.com'
-  }
-  render(<Blog blog={blog} />)
+  let blog
+
+  beforeEach(() => {
+    blog = {
+      title: 'Test title',
+      author: 'Tester Author',
+      likes: 0,
+      user: {
+        name: 'Tester is the name',
+        username: 'Tester is the username',
+        id: '123123'
+      },
+      url: 'example@url.com'
+    }
+    render(<Blog blog={blog} user={blog.user}/>)
+  })
 
   test('renders blog title and author', () => {
     const element = screen.getByText('Test title Tester Author')
@@ -30,6 +35,16 @@ describe('blog component', () => {
   test('does not render likes by default', () => {
     const likes = screen.queryByText(blog.url)
     expect(likes).toBeNull()
+  })
+
+  test('url and likes are shown when the view butoon is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const nameElement = screen.getByText('Tester is the name')
+    expect(nameElement).toBeDefined()
+    const likesElement = screen.queryByText('likes')
+    expect(likesElement).toBeDefined()
   })
 
 })
