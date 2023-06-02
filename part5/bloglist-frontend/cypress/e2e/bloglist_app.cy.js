7734afc1dc4feb9b0105c57cffa28fe986012f4f
Smile <smailbestybay@gmail.com>
2023-06-02 00:31:52 -0700

    add testing route to backend

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,10 @@
 describe('Bloglist app', function() {
-  it('front page can be opened', function () {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
     cy.contains('log in to application')
   })
 })
