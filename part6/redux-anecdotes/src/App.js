899bb623a712ba523a7867a39bfbcdfeb77216fb
Smile <smailbestybay@gmail.com>
2023-06-07 19:25:58 -0700

    complete exerice 6.4

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { addVote } from './reducers/anecdoteReducer'
+import { addVote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,6 +8,13 @@ const App = () => {
   const vote = (id) => {
     // console.log('vote', id)
     dispatch(addVote(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
   }
 
   return (
@@ -25,9 +32,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,6 +24,8 @@ const reducer = (state = initialState, action) => {
   // console.log('action', action)
 
   switch(action.type){
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
     case 'ADD_VOTE':
         const id = action.payload.id
         const anecdoteToChange = state.find(n => n.id === id)
@@ -41,6 +43,13 @@ export const addVote = (id) => {
   return {
     type: 'ADD_VOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: asObject(content)
   }
 }
 
