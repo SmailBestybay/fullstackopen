4d2cc50c9d275e78f9a1d004a730a699599d2f5b
Smile <smailbestybay@gmail.com>
2023-06-07 19:14:43 -0700

    complete exercise 6.3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { addVote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    // console.log('vote', id)
+    dispatch(addVote(id))
   }
 
   return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,28 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  // console.log('state now: ', state)
+  // console.log('action', action)
 
-  return state
+  switch(action.type){
+    case 'ADD_VOTE':
+        const id = action.payload.id
+        const anecdoteToChange = state.find(n => n.id === id)
+        const changedAnecdote = {
+          ...anecdoteToChange,
+          votes: anecdoteToChange.votes + 1
+        }
+        return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    default:
+      return state
+  }
+}
+
+export const addVote = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
