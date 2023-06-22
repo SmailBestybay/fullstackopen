b91001bc535de93ae6f1912befbd07b5c4dd5c67
Smile <smailbestybay@gmail.com>
2023-06-22 01:00:07 -0700

    add prettier to client

--- part7/extended-bloglis-redux/client/src/App.js
+++ part7/extended-bloglis-redux/client/src/App.js
@@ -1,88 +1,95 @@
-import { useState, useEffect } from 'react'
-import Blogs from './components/Blogs'
-import LoginForm from './components/LoginForm'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blogs from "./components/Blogs";
+import LoginForm from "./components/LoginForm";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [messageStatus, setMessageStatus] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [messageStatus, setMessageStatus] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
+  }, []);
 
   const hanldeLogout = () => {
-    window.localStorage.removeItem('loggedBlogAppUser')
-    setUser(null)
-  }
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setUser(null);
+  };
 
-  const handleLike =  async (blog) => {
+  const handleLike = async (blog) => {
     const newBlog = {
       user: blog.user.id,
       likes: blog.likes + 1,
       author: blog.author,
       title: blog.title,
       url: blog.url,
-    }
-    const updatedBlog = await blogService.update(blog.id, newBlog)
-    const newBlogs = blogs.map(b => b.id === updatedBlog.id ? updatedBlog : b)
-    setBlogs(newBlogs)
-  }
+    };
+    const updatedBlog = await blogService.update(blog.id, newBlog);
+    const newBlogs = blogs.map((b) =>
+      b.id === updatedBlog.id ? updatedBlog : b
+    );
+    setBlogs(newBlogs);
+  };
 
   const handleRemove = async (blog) => {
     if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
-      blogService.remove(blog.id)
-      const newBlogs = blogs.filter(b => b.id !== blog.id)
-      setBlogs(newBlogs)
+      blogService.remove(blog.id);
+      const newBlogs = blogs.filter((b) => b.id !== blog.id);
+      setBlogs(newBlogs);
     }
-  }
+  };
 
   const createBlog = async (newBlog) => {
-    await blogService.create(newBlog)
+    await blogService.create(newBlog);
 
-    setBlogs(await blogService.getAll())
-  }
+    setBlogs(await blogService.getAll());
+  };
 
   const notify = (notifyMessage, notifyMessageType) => {
-    setMessage(notifyMessage)
-    setMessageStatus(notifyMessageType)
+    setMessage(notifyMessage);
+    setMessageStatus(notifyMessageType);
     setTimeout(() => {
-      setMessage(null)
-      setMessageStatus(null)
-    }, 5000)
-  }
+      setMessage(null);
+      setMessageStatus(null);
+    }, 5000);
+  };
 
   return (
     <>
-
-      {user === null && <LoginForm
-        setUser={setUser}
-        message={message}
-        messageStatus={messageStatus}
-        notify={notify}
-      />}
-      {user !== null && <Blogs
-        user={user} hanldeLogout={hanldeLogout}
-        blogs={blogs} notify={notify}
-        message={message} messageStatus={messageStatus}
-        handleLike={handleLike} handleRemove={handleRemove} createBlog={createBlog}
-      />}
-
+      {user === null && (
+        <LoginForm
+          setUser={setUser}
+          message={message}
+          messageStatus={messageStatus}
+          notify={notify}
+        />
+      )}
+      {user !== null && (
+        <Blogs
+          user={user}
+          hanldeLogout={hanldeLogout}
+          blogs={blogs}
+          notify={notify}
+          message={message}
+          messageStatus={messageStatus}
+          handleLike={handleLike}
+          handleRemove={handleRemove}
+          createBlog={createBlog}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
