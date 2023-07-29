fd4734c1e9e3b5874ccb84b3e6be4ddce9eeaf62
Smile <smailbestybay@gmail.com>
2023-07-28 21:42:44 -0700

    pull out blogs into home component

--- part7/extended-bloglist-query/client/src/App.js
+++ part7/extended-bloglist-query/client/src/App.js
@@ -1,35 +1,21 @@
-import { useRef } from "react";
-import Blog from "./components/Blog";
-import blogService from "./services/blogs";
 import loginService from "./services/login";
 import storageService from "./services/storage";
 
 import LoginForm from "./components/Login";
-import NewBlog from "./components/NewBlog";
 import Notification from "./components/Notification";
-import Togglable from "./components/Togglable";
+
+import Home from "./views/Home";
 
 import { useNotificationDispatch } from "./contexts/NotificationContext";
 import { useUserValue, useUserDispatch } from "./contexts/UserContext";
-import { useQuery } from "react-query";
-import {
-  useNewBlogMutation,
-  useUpdateBlogMutation,
-  useDeleteBlogMutation,
-} from "./mutations/blogMutations";
+
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   const notificationDispatch = useNotificationDispatch();
-  const blogsQuery = useQuery("blogs", blogService.getAll);
 
   const userDispatch = useUserDispatch();
   const user = useUserValue();
-
-  const newBlogMutation = useNewBlogMutation();
-  const updateBlogMutation = useUpdateBlogMutation();
-  const deleteBlogMutation = useDeleteBlogMutation();
-
-  const blogFormRef = useRef();
 
   const notifyWith = (message, status = "info") => {
     notificationDispatch({
@@ -60,28 +46,6 @@ const App = () => {
     notifyWith("logged out");
   };
 
-  const createBlog = async (newBlog) => {
-    newBlogMutation.mutate(newBlog);
-    notifyWith(`A new blog '${newBlog.title}' by '${newBlog.author}' added`);
-    blogFormRef.current.toggleVisibility();
-  };
-
-  const like = async (blog) => {
-    const blogToUpdate = { ...blog, likes: blog.likes + 1, user: blog.user.id };
-    updateBlogMutation.mutate(blogToUpdate);
-    notifyWith(`A like for the blog '${blog.title}' by '${blog.author}'`);
-  };
-
-  const remove = async (blog) => {
-    const ok = window.confirm(
-      `Sure you want to remove '${blog.title}' by ${blog.author}`
-    );
-    if (ok) {
-      deleteBlogMutation.mutate(blog.id);
-      notifyWith(`The blog' ${blog.title}' by '${blog.author} removed`);
-    }
-  };
-
   if (!user) {
     return (
       <div>
@@ -92,8 +56,6 @@ const App = () => {
     );
   }
 
-  const byLikes = (b1, b2) => b2.likes - b1.likes;
-
   return (
     <div>
       <h2>blogs</h2>
@@ -102,24 +64,7 @@ const App = () => {
         {user.name} logged in
         <button onClick={logout}>logout</button>
       </div>
-      <Togglable buttonLabel="new note" ref={blogFormRef}>
-        <NewBlog createBlog={createBlog} />
-      </Togglable>
-      {blogsQuery.isLoading ? (
-        <div>loading blogs...</div>
-      ) : (
-        <div>
-          {blogsQuery.data.sort(byLikes).map((blog) => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              like={() => like(blog)}
-              canRemove={user && blog.user.username === user.username}
-              remove={() => remove(blog)}
-            />
-          ))}
-        </div>
-      )}
+      <Home notifyWith={notifyWith} />
     </div>
   );
 };
---
+++ part7/extended-bloglist-query/client/src/views/Home.js
@@ -0,0 +1,74 @@
+import blogService from "../services/blogs";
+import { useQuery } from "react-query";
+import { useRef } from "react";
+
+import Togglable from "../components/Togglable";
+import NewBlog from "../components/NewBlog";
+import Blog from "../components/Blog";
+
+import {
+  useNewBlogMutation,
+  useUpdateBlogMutation,
+  useDeleteBlogMutation,
+} from "../mutations/blogMutations";
+
+import { useUserValue } from "../contexts/UserContext";
+
+const Home = ({ notifyWith }) => {
+  const blogsQuery = useQuery("blogs", blogService.getAll);
+
+  const user = useUserValue();
+
+  const newBlogMutation = useNewBlogMutation();
+  const updateBlogMutation = useUpdateBlogMutation();
+  const deleteBlogMutation = useDeleteBlogMutation();
+
+  const blogFormRef = useRef();
+
+  const createBlog = async (newBlog) => {
+    newBlogMutation.mutate(newBlog);
+    notifyWith(`A new blog '${newBlog.title}' by '${newBlog.author}' added`);
+    blogFormRef.current.toggleVisibility();
+  };
+
+  const like = async (blog) => {
+    const blogToUpdate = { ...blog, likes: blog.likes + 1, user: blog.user.id };
+    updateBlogMutation.mutate(blogToUpdate);
+    notifyWith(`A like for the blog '${blog.title}' by '${blog.author}'`);
+  };
+
+  const remove = async (blog) => {
+    const ok = window.confirm(
+      `Sure you want to remove '${blog.title}' by ${blog.author}`
+    );
+    if (ok) {
+      deleteBlogMutation.mutate(blog.id);
+      notifyWith(`The blog' ${blog.title}' by '${blog.author} removed`);
+    }
+  };
+  const byLikes = (b1, b2) => b2.likes - b1.likes;
+  return (
+    <>
+      <Togglable buttonLabel="new note" ref={blogFormRef}>
+        <NewBlog createBlog={createBlog} />
+      </Togglable>
+      {blogsQuery.isLoading ? (
+        <div>loading blogs...</div>
+      ) : (
+        <div>
+          {blogsQuery.data.sort(byLikes).map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              like={() => like(blog)}
+              canRemove={user && blog.user.username === user.username}
+              remove={() => remove(blog)}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
